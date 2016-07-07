ba45c4756655f98f631d67567549c58f214e6825
Shivam Satija <dr.droid27@gmail.com>
2016-07-07 17:42:54 +0530

    some secret files

--- app/src/main/java/com/algowire/chanelview/MainActivity.java
+++ app/src/main/java/com/algowire/chanelview/MainActivity.java
@@ -13,8 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.Switch;
 
 import java.util.Random;
 
